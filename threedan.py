69c9f3b4af1301fb283dcd1bccff79bbe7287955
JB <jblack25@students.towson.edu>
2016-09-07 15:12:39 -0400

    Update threedan.py
    cleaned up, added functions, proper commenting etc.

--- threedan.py
+++ threedan.py
@@ -3,53 +3,61 @@ import optparse
 import shodan
 import time
 
-print '''
-######## ##     ## ########  ######## ######## ########     ###    ##    ##
-   ##    ##     ## ##     ## ##       ##       ##     ##   ## ##   ###   ##
-   ##    ##     ## ##     ## ##       ##       ##     ##  ##   ##  ####  ##
-   ##    ######### ########  ######   ######   ##     ## ##     ## ## ## ##
-   ##    ##     ## ##   ##   ##       ##       ##     ## ######### ##  ####
-   ##    ##     ## ##    ##  ##       ##       ##     ## ##     ## ##   ###
-   ##    ##     ## ##     ## ######## ######## ########  ##     ## ##    ##
-                                            
-'''
+def search(query,filename):
 
-print ' by threebones \n https://github.com/threebarber\n'
+    SHODAN_API_KEY = "SHODAN API KEY HERE" #api key to utilize shodan API
+    api = shodan.Shodan(SHODAN_API_KEY) #create api shodan object
+
+    try:
+        results = api.search(query) #try to search using api object, error handling for messy shodan.APIERROR errors
+    except shodan.APIError, se:
+        print "[-] Shodan APIError, " +str(se)
+
+    print '[+] Saving to: '        +str(filename) #display progress and user-selected options back to user
+    time.sleep(5)
+    print '[+] Searching for: '    +(query)
+    time.sleep(5)
+    print '[+] Total Results: '    +str(results['total']) + "\n-----------------------------------"
+    time.sleep(5)
+
+    for result in results['matches']: #print list of returned IP's
+        print str(result['ip_str'])
+        list = open (filename,'a') #write retrieved ip's to text document defined by user
+        list.write(str(result['ip_str'] + '\n'))
+
+def main():
+    print '''
+    ######## ##     ## ########  ######## ######## ########     ###    ##    ##
+     ##    ##     ## ##     ## ##       ##       ##     ##   ## ##   ###   ##
+    ##    ##     ## ##     ## ##       ##       ##     ##  ##   ##  ####  ##
+    ##    ######### ########  ######   ######   ##     ## ##     ## ## ## ##
+    ##    ##     ## ##   ##   ##       ##       ##     ## ######### ##  ####
+    ##    ##     ## ##    ##  ##       ##       ##     ## ##     ## ##   ###
+    ##    ##     ## ##     ## ######## ######## ########  ##     ## ##    ##
+    '''
+    print ' by threebones \n https://github.com/threebarber\n'
 
 
-parser = optparse.OptionParser()
+    parser = optparse.OptionParser() #create parser object called "parser"
 
-parser.usage = "[+] Usage:   threedan.py -q <query> -f <filename.txt>" \
-               "\n[+] Example: threedan.py -q ftp  -f ftplist.txt      "
+    parser.usage = "[+] Usage:   threedan.py -q <query> -f <filename.txt>" \
+                   "\n[+] Example: threedan.py -q ftp  -f ftplist.txt      " #add usage for "parser" object as well as example
 
-parser.add_option(
-        '-q','--query',dest='query',type='string',help='specify search query IE \'ftp\'')
+    parser.add_option(
+            '-q','--query',dest='query',type='string',help='specify search query IE \'ftp\'') #add search query option as -q
 
-parser.add_option(
-        '-f','--file',dest='filename',default='list.txt',type='string',help='specify filename (add .txt to end)')
+    parser.add_option(
+            '-f','--file',dest='filename',default='list.txt',type='string',help='specify filename (add .txt to end)') #add filename option as -f
 
-(options, args) = parser.parse_args(sys.argv)
+    (options, args) = parser.parse_args(sys.argv) #finalize parsing portion
 
-SHODAN_API_KEY = "INSERT SHODAN API KEY HERE"
-api = shodan.Shodan(SHODAN_API_KEY)
+    query = options.query
+    filename = options.filename
 
-if (options.query) == None != (options.filename == None):
-    print parser.usage
-    exit(0)
-try:
-    results = api.search(options.query)
-except shodan.APIError, se:
-    print "[-] Shodan APIError, " +str(se)
+    if (query) == None != (filename == None):
+        print parser.usage
+        exit(0) #check to make sure required params were assigned a value - if not, exit
+    search(query,filename)
 
-print '[+] Saving to: '        +options.filename
-time.sleep(5)
-print '[+] Searching for: '    +options.query
-time.sleep(5)
-print '[+] Total Results: '    +str(results['total']) + "\n"
-time.sleep(5)
-
-for result in results['matches']:
-    print str(result['ip_str'])
-    list = open (options.filename,'a')
-    list.write(str(result['ip_str'] + '\n'))
-
+if __name__ == '__main__':
+    main()
