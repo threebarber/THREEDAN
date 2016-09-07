f52eb627dbb806dbc23b941f5d04f147bbbceb05
JB <jblack25@students.towson.edu>
2016-09-07 12:02:37 -0400

    Update threedan.py

--- threedan.py
+++ threedan.py
@@ -1,9 +1,9 @@
 import sys
 import optparse
 import shodan
-from colorama import Fore, Back, Style
+import time
 
-print (Fore.RED + '''
+print '''
 ######## ##     ## ########  ######## ######## ########     ###    ##    ##
    ##    ##     ## ##     ## ##       ##       ##     ##   ## ##   ###   ##
    ##    ##     ## ##     ## ##       ##       ##     ##  ##   ##  ####  ##
@@ -11,15 +11,15 @@ print (Fore.RED + '''
    ##    ##     ## ##   ##   ##       ##       ##     ## ######### ##  ####
    ##    ##     ## ##    ##  ##       ##       ##     ## ##     ## ##   ###
    ##    ##     ## ##     ## ######## ######## ########  ##     ## ##    ##
-
 '''
 
-       ) + (Fore.GREEN + ' by threebones \n https://github.com/threebarber')
-
-
+print ' by threebones \n https://github.com/threebarber\n'
 
 
 parser = optparse.OptionParser()
+
+parser.usage = "[+] Usage:   threedan.py -q <query> -f <filename.txt>" \
+               "\n[+] Example: threedan.py -q ftp  -f ftplist.txt      "
 
 parser.add_option(
         '-q','--query',dest='query',type='string',help='specify search query IE \'ftp\'')
@@ -32,14 +32,23 @@ parser.add_option(
 SHODAN_API_KEY = "INSERT SHODAN API KEY HERE"
 api = shodan.Shodan(SHODAN_API_KEY)
 
-results = api.search(options.query)
-print (Style.BRIGHT + '[+] Saving to: ') +options.filename
-print (Style.BRIGHT +'[+] Searching for: ') +options.query
-print (Style.BRIGHT + '[+] Total Results: ') +str(results['total']) + "\n"
+if (options.query) == None != (options.filename == None):
+    print parser.usage
+    exit(0)
+try:
+    results = api.search(options.query)
+except shodan.APIError, se:
+    print "[-] Shodan APIError, " +str(se)
+
+print '[+] Saving to: '        +options.filename
+time.sleep(5)
+print '[+] Searching for: '    +options.query
+time.sleep(5)
+print '[+] Total Results: '    +str(results['total']) + "\n"
+time.sleep(5)
 
 for result in results['matches']:
     print str(result['ip_str'])
     list = open (options.filename,'a')
     list.write(str(result['ip_str'] + '\n'))
-
 
